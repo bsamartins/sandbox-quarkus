817b04dc36a3da9a9cb4547c31a264556472fb67
Bernardo Martins <bernardo.martins@utmost.co>
2021-11-30 15:33:14 +0000

    Add heroes resolver

--- graphql/src/main/kotlin/io/bsamartins/sandbox/quarkus/graphql/FilmResource.kt
+++ graphql/src/main/kotlin/io/bsamartins/sandbox/quarkus/graphql/FilmResource.kt
@@ -1,5 +1,9 @@
 package io.bsamartins.sandbox.quarkus.graphql
 
+import io.smallrye.mutiny.Uni
+import io.smallrye.mutiny.coroutines.asUni
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.eclipse.microprofile.graphql.Description
 import org.eclipse.microprofile.graphql.GraphQLApi
 import org.eclipse.microprofile.graphql.Query
@@ -10,9 +14,11 @@ class FilmResource(private val galaxyService: GalaxyService) {
 
     @Description("Get all Films from a galaxy far far away")
     @Query("allFilms")
-    fun allFilms(): List<Film> = galaxyService.allFilms
+    fun allFilms(): Uni<List<Film>> {
+        return runBlocking { async { galaxyService.allFilms } }.asUni()
+    }
 
-    fun heroes(@Source film: Film): List<Hero> {
-        return galaxyService.getHeroesByFilm(film)
+    fun heroes(@Source film: Film): Uni<List<Hero>> {
+        return runBlocking { async { galaxyService.getHeroesByFilm(film) } }.asUni()
     }
 }
