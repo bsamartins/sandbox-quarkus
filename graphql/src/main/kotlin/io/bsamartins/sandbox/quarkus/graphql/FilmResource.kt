dc0ac30851d76236afea8dfd9f03c08f58eb37b8
Bernardo Martins <bernardo.martins@utmost.co>
2021-11-30 19:46:59 +0000

    Add mutations and endpoints

--- graphql/src/main/kotlin/io/bsamartins/sandbox/quarkus/graphql/FilmResource.kt
+++ graphql/src/main/kotlin/io/bsamartins/sandbox/quarkus/graphql/FilmResource.kt
@@ -3,8 +3,10 @@ package io.bsamartins.sandbox.quarkus.graphql
 import io.smallrye.mutiny.Uni
 import org.eclipse.microprofile.graphql.Description
 import org.eclipse.microprofile.graphql.GraphQLApi
+import org.eclipse.microprofile.graphql.Mutation
 import org.eclipse.microprofile.graphql.Query
 import org.eclipse.microprofile.graphql.Source
+
 
 @GraphQLApi
 class FilmResource(private val galaxyService: GalaxyService) {
@@ -13,6 +15,27 @@ class FilmResource(private val galaxyService: GalaxyService) {
     @Query("allFilms")
     fun allFilms(): Uni<List<Film>> {
         return galaxyService.allFilms()
+    }
+
+    @Query
+    fun getHeroesWithSurname(surname: String): Uni<List<Hero>> {
+        return galaxyService.getHeroesBySurname(surname)
+    }
+
+    @Query
+    @Description("Get a Film from a galaxy far far away")
+    fun getFilm(filmId: Int): Uni<Film> {
+        return galaxyService.getFilm(filmId)
+    }
+
+    @Mutation
+    fun createHero(hero: Hero): Uni<Hero> {
+        return galaxyService.addHero(hero)
+    }
+
+    @Mutation
+    fun deleteHero(id: Int): Uni<Hero> {
+        return galaxyService.deleteHero(id)
     }
 
     fun heroes(@Source film: Film): Uni<List<Hero>> {
