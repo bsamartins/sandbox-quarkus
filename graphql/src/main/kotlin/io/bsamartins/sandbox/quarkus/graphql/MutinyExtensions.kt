7c53d03d502cb9da0917acf27b0b637214c15542
Bernardo Martins <bernardo.martins@utmost.co>
2021-11-30 19:29:15 +0000

    Clean up unused extension

--- graphql/src/main/kotlin/io/bsamartins/sandbox/quarkus/graphql/MutinyExtensions.kt
+++ graphql/src/main/kotlin/io/bsamartins/sandbox/quarkus/graphql/MutinyExtensions.kt
@@ -1,8 +1,5 @@
 package io.bsamartins.sandbox.quarkus.graphql
 
-import io.smallrye.mutiny.Multi
 import io.smallrye.mutiny.Uni
 
 fun <T : Any> T.asUni(): Uni<T> = Uni.createFrom().item(this)
-
-fun <T : Any> Iterable<T>.asMulti(): Multi<T> = Multi.createFrom().iterable(this)
