47e3b453f2e440ee04def3b0ab563794b33f8799
Bernardo Martins <bernardo.martins@utmost.co>
2021-11-30 11:20:24 +0000

    Refactor build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,48 @@
 plugins {
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.allopen") version "1.5.31"
-    id("io.quarkus")
-}
-
-repositories {
-    mavenCentral()
-    mavenLocal()
-}
-
-val quarkusVersion: String by project
-
-dependencies {
-    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:$quarkusVersion"))
-    implementation("io.quarkus:quarkus-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.quarkus:quarkus-arc")
-    testImplementation("io.quarkus:quarkus-junit5")
+    kotlin("jvm") apply false
 }
 
 group = "io.bsamartins.sandbox.quarkus"
 version = "1.0-SNAPSHOT"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.allopen")
+    apply(plugin = "io.quarkus")
+
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
+
+    dependencies {
+        val implementation by configurations
+        val testImplementation by configurations
+        val quarkusVersion: String by project
+        val kotlinVersion: String by project
+
+        implementation(platform("org.jetbrains.kotlin:kotlin-bom:$kotlinVersion"))
+        implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:$quarkusVersion"))
+        implementation("io.quarkus:quarkus-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("io.quarkus:quarkus-arc")
+        testImplementation("io.quarkus:quarkus-junit5")
+    }
+
+    configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+        kotlinOptions.javaParameters = true
+    }
 }
 
-allOpen {
-    annotation("javax.ws.rs.Path")
-    annotation("javax.enterprise.context.ApplicationScoped")
-    annotation("io.quarkus.test.junit.QuarkusTest")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
-    kotlinOptions.javaParameters = true
-}
+//allOpen {
+//    annotation("javax.ws.rs.Path")
+//    annotation("javax.enterprise.context.ApplicationScoped")
+//    annotation("io.quarkus.test.junit.QuarkusTest")
+//}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,14 @@
 pluginManagement {
     val quarkusVersion: String by settings
+    val kotlinVersion: String by settings
     repositories {
         mavenLocal()
         mavenCentral()
         gradlePluginPortal()
     }
     plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.allopen") version kotlinVersion
         id("io.quarkus") version quarkusVersion
     }
 }
